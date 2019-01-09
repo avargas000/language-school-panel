046528a806b4f8e58d88e4be1eeb2b946c51a571
Ana <a.vargas2155@gmail.com>
2019-01-09 21:50:34 +0000

    styling

--- app/controllers/cohorts_controller.rb
+++ app/controllers/cohorts_controller.rb
@@ -4,39 +4,39 @@ class CohortsController < ApplicationController
         @cohort = Cohort.all.reverse
         @new_cohort = cohort.new
       end
-    
+
       def create
         @temp = Cohort.create(cohort_params)
         if @temp.valid?
-          flash[:notice] = "cohort CREATED!"
+          flash[:notice] = "COHORT CREATED!"
         else
           flash[:notice] = "YOU TYPED TOO MUCH!"
         end
         redirect_to '/cohorts'
       end
-    
+
       def edit
         @cohort = Cohort.find(params[:id])
       end
-    
+
       def update
         @cohort = Cohort.find(params[:id])
         @cohort.update(cohort_params)
         @cohort.save
       end
-    
+
       # /post/:id
       def destroy
         @cohort = Cohort.find(params[:id])
         @cohort.destroy
         redirect_to '/cohorts'
       end
-    
+
       private
-    
+
       def cohort_params
         params.require(:cohort).permit(:cohort_name, :start_date, :end_date)
       end
-    
+
 
 end
